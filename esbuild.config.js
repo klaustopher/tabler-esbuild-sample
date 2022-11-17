8595fe1879f1f4402bca1a38b6341558314dc05b
Klaus Zanders <code@kgz.me>
2022-11-17 18:00:23 +0100

    Simple Example

---
+++ esbuild.config.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const { build } = require("esbuild");
+const { sassPlugin } = require("esbuild-sass-plugin");
+
+build({
+  entryPoints: ["styles/application.scss"],
+  bundle: true,
+  logLevel: "info",
+  outdir: "builds",
+  plugins: [sassPlugin()],
+})
+  .then(() => console.log("⚡ Build complete! ⚡"))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
